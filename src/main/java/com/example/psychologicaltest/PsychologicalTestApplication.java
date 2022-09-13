3f8831380a2e89412f93f2f439aba7b422b30fff
user <tjdgn1125@gmail.com>
2022-09-13 10:57:34 +0900

    v0.1 first commit

---
+++ src/main/java/com/example/psychologicaltest/PsychologicalTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.psychologicaltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsychologicalTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsychologicalTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/psychologicaltest/PsychologicalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.psychologicaltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsychologicalTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
