bb84a7b5841f06a6db8dbce019ed402963a80fc1
Logan <redkal19@gmail.com>
2022-09-13 22:18:15 +0900

    v0.2 ys 프론트 기능구현 중

---
+++ src/main/java/controller/PtController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@Controller
+public class PtController {
+
+//    @PostMapping("/ystest")
+//    public String madeByYs(Model model){
+//        model.addAttribute("data", "hello!!");
+//
+//        return "/ystest";
+//    }
+
+}
