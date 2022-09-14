fd31fba182ba329c596456ef51a2d1869a9a15b6
Logan <redkal19@gmail.com>
2022-09-14 14:16:38 +0900

    v0.3.1 ys 프론트 기능구현 중  - 결과값 알고리즘 개선 진행

--- src/main/java/controller/PtController.java
+++ src/main/java/controller/PtController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class PtController {
 
+    //임시로 주석처리
 //    @PostMapping("/ystest")
 //    public String madeByYs(Model model){
 //        model.addAttribute("data", "hello!!");
