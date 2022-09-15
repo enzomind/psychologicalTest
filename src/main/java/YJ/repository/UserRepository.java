5bc636286f93ada24cf23b7e6ac387bbc1ff0b74
Hyongyi <102146170+Hyongyi@users.noreply.github.com>
2022-09-15 17:37:05 +0900

    v0.5.3 hy test commit

--- src/main/java/YJ/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package YJ.repository;
-
-import YJ.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {
-}
