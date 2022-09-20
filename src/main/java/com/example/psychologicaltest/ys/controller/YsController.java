8014825e94c6fa3bca9a76df2841aaca37f69c67
Hyongyi <102146170+Hyongyi@users.noreply.github.com>
2022-09-20 16:26:48 +0900

    v2.5 hy test commit

--- src/main/java/com/example/psychologicaltest/ys/controller/YsController.java
+++ src/main/java/com/example/psychologicaltest/ys/controller/YsController.java
@@ -1,44 +1,49 @@
 package com.example.psychologicaltest.YS.controller;
 
-import com.example.psychologicaltest.YS.dto.YsmemberRequestDTO;
+import com.example.psychologicaltest.YS.dto.YsmemberDTO;
 import com.example.psychologicaltest.YS.dto.YstestDTO;
 import com.example.psychologicaltest.YS.entity.Ysmember;
 import com.example.psychologicaltest.YS.entity.Ystest;
 import com.example.psychologicaltest.YS.repository.YsmemberRepository;
 import com.example.psychologicaltest.YS.repository.YstestRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequiredArgsConstructor
+@Log4j2
 public class YsController {
 
     private final YstestRepository ystestRepository;
     private final YsmemberRepository ysmemberRepository;
+
 
     @RequestMapping(value = "/insertResult", method = RequestMethod.POST)
     public void insertResult(YstestDTO ystestDTO) throws Exception {
 
         Ystest ystest = Ystest.builder()
                 .tresult(ystestDTO.getTresult())
+                .tcode(ystestDTO.getTcode())
                 .build();
 
         ystestRepository.save(ystest);
-
     }
 
     @RequestMapping(value = "/insertMember", method = RequestMethod.POST)
-    public void insertMemberInfo(YsmemberRequestDTO ysmemberRequestDTO) throws Exception {
+    public void insertMemberInfo(YsmemberDTO ysmemberDTO) throws Exception {
 
         Ysmember ysmember = Ysmember.builder()
-                .mname(ysmemberRequestDTO.getMname())
-                .mgender(ysmemberRequestDTO.getMgender())
-                .mage(ysmemberRequestDTO.getMage())
+                .mname(ysmemberDTO.getMname())
+                .mgender(ysmemberDTO.getMgender())
+                .mage(ysmemberDTO.getMage())
+                .mresult(ysmemberDTO.getMresult())
                 .build();
 
         ysmemberRepository.save(ysmember);
 
     }
+
 }
+
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -2,14 +2,14 @@ $(document).ready(function () {
     $("#wrap>#main_banner>#slide>.slide_banner").hover(function () {
         $(this).find("img").css("box-shadow", "5px 5px 5px gray");
     },  function () {
-        $(this).find("img").css("transform", "scale(1.0)","transition", "0.5s");
+        $(this).find("img").css("box-shadow", "none");
     });
 
     $("#content>.link>.test").hover(function () {
         $(this).css("transform", "scale(1.1)");
     },  function () {
         $(this).css("transform", "scale(1.0)");
-        
+
     });
 
     $("#left").click(function() {
@@ -23,7 +23,7 @@ $(document).ready(function () {
     $("#right").click(function() {
         var right = 0;
         right++;
-        var next1= right * (-900) +'px';
+        var next1= right * (-750) +'px';
         $("#slide").animate( {
            left: next1
           }, 1000 );
